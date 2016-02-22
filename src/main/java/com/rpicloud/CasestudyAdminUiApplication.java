3d10e68420de03dc11a81264845da40553702aa8
Kasper Nissen <kni@it-minds.dk>
2016-02-22 21:18:16 +0100

    Project added

---
+++ src/main/java/com/rpicloud/CasestudyAdminUiApplication.java
@@ -0,0 +1,12 @@
+package com.rpicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyAdminUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudyAdminUiApplication.class, args);
+	}
+}
